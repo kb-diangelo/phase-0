d2a8ba809d8e198a44349eb1ec18b26798d03de5
K. B. DiAngelo <KDiAngelo@gmail.com>
2015-11-19 19:18:14 +0000

    add 5.3 file

---
+++ week-5/group-research-methods/my_solution.rb
@@ -0,0 +1,94 @@
+# Research Methods
+
+# I spent .75 hours on this challenge.
+
+i_want_pets = ["I", "want", 3, "pets", "but", "only", "have", 2]
+my_family_pets_ages = {"Evi" => 6, "Ditto" => 3, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, "Annabelle" => 0}
+
+# Person 1's solution
+def my_array_finding_method(source, thing_to_find)
+  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+end
+
+def my_hash_finding_method(source, thing_to_find)
+  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+end
+
+# Identify and describe the Ruby method(s) you implemented.
+#
+#
+#
+
+# Person 2
+def my_array_modification_method!(source, thing_to_modify)
+  source.dup # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+end
+
+def my_hash_modification_method!(source, thing_to_modify)
+  source.dup # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+end
+
+# Identify and describe the Ruby method(s) you implemented.
+#
+#
+#
+
+# Person 3 - KB DIANGELO
+def my_array_sorting_method(source)
+  return source.sort {|item1, item2| item1.to_s <=> item2.to_s}
+end
+
+def my_hash_sorting_method(source)
+  return source.sort_by {|key, value| value}
+end
+
+# Identify and describe the Ruby method(s) you implemented.
+#
+# .to_s : converts an object (in this case a fixnum) to a string
+# .sort { |a, b| block comparing a and b with <=>} : uses the block provided to compare each item to its neighbor and return whether it comes before or after
+# .sort_by { |a, b| a block telling it which to sort by} : uses the block provided to sort by what is specified
+# 
+
+# Person 4
+def my_array_deletion_method!(source, thing_to_delete)
+  source.dup # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+end
+
+def my_hash_deletion_method!(source, thing_to_delete)
+  source.dup # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+end
+
+# Identify and describe the Ruby method(s) you implemented.
+#
+#
+#
+
+
+# Person 5
+def my_array_splitting_method(source)
+  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+end
+
+def my_hash_splitting_method(source, age)
+  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+end
+
+# Identify and describe the Ruby method(s) you implemented.
+#
+#
+#
+
+
+# Release 1: Identify and describe the Ruby method you implemented. Teach your
+# accountability group how to use the methods.
+#
+#
+#
+
+
+# Release 3: Reflect!
+# What did you learn about researching and explaining your research to others?
+#
+#
+#
+#
